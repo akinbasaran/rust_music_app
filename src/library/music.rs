bb7888865cb542d0c0bb14d4bfc153ef111c4e5e
AKIN BAŞARAN <33937968+akinbasaran@users.noreply.github.com>
2023-12-17 12:35:42 +0300

    the end

--- src/library/music.rs
+++ src/library/music.rs
@@ -1,52 +1,120 @@
-pub struct Music<T> {
-    title: T,
-    like: bool,
-    unlike: bool,
+#[derive(Debug, Clone)]
+pub struct Music {
+    title: String,
+    like: u32,
+    unlike: u32,
+    now_playing: bool,
 }
 
-impl<T> Music<T> {
-    pub fn new(title: T, like: bool, unlike: bool) -> Self {
-        Self { title, like, unlike }
+impl Music {
+    pub fn new(title: String, like: u32, unlike: u32) -> Self {
+        Self { title, like, unlike, now_playing: false }
     }
 }
 
-pub struct Playlist<T> {
-    list: Vec<Music<T>>,
-    now_playing: Option<Music<T>>,
+pub struct Playlist {
+    list: Vec<Music>,
 }
 
-impl<T> Playlist<T> {
+impl Playlist {
     pub fn new() -> Self {
-        Self { list: Vec::new(), now_playing: None }
+        Self { list: Vec::new() }
     }
 
-    // add aynı eklenemeyecek filter kullanabılırsın
-    pub fn add() {
-        let a = [1, 2, 3,4];
-
-        assert_eq!(
-            a.iter().find(|&&x| x == 2),
-            Some(&2)
-        );
-
-        assert_eq!(
-            a.iter().find(|&&x| x == 5),
-            None
-        );
+    // add
+    pub fn add(&mut self, music: Music) {
+        self.list.push(music);
+        //aynısı varsa tekrar eklenmeyecek kontrol eklenecek buraya
     }
     // remove
-
-    // play olacak now playıngten
-
-    // sonraki şarkı
-
-    // önceki şarkı
-
+    pub fn remove(&mut self, index: usize) {
+        if index < self.list.len() {
+            self.list.remove(index);
+        } else {
+            panic!("index is not available")
+        }
+    }
+    // play
+    pub fn play(&mut self, index: usize) {
+        if index < self.list.len() {
+            self.list[index].now_playing = true;
+        } else {
+            panic!("index is not available")
+        }
+    }
     // durdur
-
-    // like boole
-
-    // unlike boole
-
+    pub fn stop(&mut self, index: usize) {
+        if index < self.list.len() {
+            self.list[index].now_playing = false;
+        } else {
+            panic!("index is not available")
+        }
+    }
+    // like
+    pub fn like_music(&mut self, index: usize) {
+        if index < self.list.len() {
+            self.list[index].like += 1;
+        } else {
+            panic!("index is not available")
+        }
+    }
+    // unlike
+    pub fn unlike_music(&mut self, index: usize) {
+        if index < self.list.len() {
+            self.list[index].like -= 1;
+        } else {
+            panic!("index is not available")
+        }
+    }
     // print list
+    pub fn print_playlist(&self) {
+        for (index, music) in self.list.iter().enumerate() {
+            println!(
+                "{}.Playing:[{}], Title:{}, Like:{}, Unlike:{}",
+                index + 1,
+                if music.now_playing {
+                    "x"
+                } else {
+                    ""
+                },
+                music.title,
+                music.like,
+                music.unlike,
+              
+            );
+        }
+        println!("---------------------------");
+    }
+    // sonraki şarkı
+    pub fn next_music(&mut self) {
+        let mut new_list = self.list.clone();
+        let mut ind = 0;
+        // let mut playing_music = self.list.iter().find(|music| music.now_playing == true);
+        for (index, item) in new_list.iter_mut().enumerate() {
+            if item.now_playing == true {
+                item.now_playing = false;
+                ind = index + 1;
+            } else {
+            }
+        }
+        new_list[ind].now_playing = true;
+        self.list = new_list;
+
+    }
+    // önceki şarkı
+    pub fn prev_music(&mut self) {
+        
+        let mut new_list = self.list.clone();
+        let mut ind = 0;
+        // let mut playing_music = self.list.iter().find(|music| music.now_playing == true);
+        for (index, item) in new_list.iter_mut().enumerate() {
+            if item.now_playing == true {
+                item.now_playing = false;
+                ind = index - 1;
+            } else {
+            }
+        }
+        new_list[ind].now_playing = true;
+        self.list = new_list;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
 mod library;
 
-use library::music::{Playlist};
+use library::music::{ Music, Playlist };
 fn main() {
-    println!("Hello, world!");
+    let mut favori_muziklerim = Playlist::new();
+    let music1 = Music::new("lovely".to_string(), 0, 0);
+    let music2 = Music::new("yarim yarim".to_string(), 0, 0);
+    let music3 = Music::new("canısı".to_string(), 0, 0);
+    let music4 = Music::new("konyalım".to_string(), 0, 0);
+
+    favori_muziklerim.add(music1);
+    favori_muziklerim.add(music2);
+    favori_muziklerim.add(music3);
+    favori_muziklerim.add(music4); 
+    favori_muziklerim.print_playlist();
+    favori_muziklerim.like_music(3);
+    favori_muziklerim.like_music(2);
+    favori_muziklerim.like_music(2);
+    favori_muziklerim.like_music(1);
+    favori_muziklerim.like_music(1);
+    favori_muziklerim.print_playlist();
+    favori_muziklerim.unlike_music(3);
+    favori_muziklerim.unlike_music(2);
+    favori_muziklerim.unlike_music(1);
+    favori_muziklerim.print_playlist();
+    favori_muziklerim.play(1);
+    favori_muziklerim.print_playlist();
+    favori_muziklerim.next_music();
+    favori_muziklerim.print_playlist();
+    favori_muziklerim.prev_music();
+    favori_muziklerim.prev_music();
+    favori_muziklerim.print_playlist();
+    favori_muziklerim.remove(3);
+    favori_muziklerim.print_playlist();
+
+
+    
 }
